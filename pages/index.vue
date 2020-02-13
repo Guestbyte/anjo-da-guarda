a666e6b6297c01389c6124f437bb39166f5cf7f9
Fernando Ortiz Mello <guestbyte@gmail.com>
2020-02-13 15:59:16 -0300

    alterações teste

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,14 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
       <div class="text-center">
         <logo />
         <vuetify-logo />
@@ -15,13 +23,18 @@
           </p>
           <p>
             For more information on Vuetify, check out the
-            <a href="https://vuetifyjs.com" target="_blank">documentation</a>.
+            <a
+              href="https://vuetifyjs.com"
+              target="_blank"
+            >documentation</a>.
           </p>
           <p>
             If you have questions, please join the official
-            <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat"
-              >discord</a
-            >.
+            <a
+              href="https://chat.vuetifyjs.com/"
+              target="_blank"
+              title="chat"
+            >discord</a>.
           </p>
           <p>
             Find a bug? Report it on the github
@@ -29,8 +42,7 @@
               href="https://github.com/vuetifyjs/vuetify/issues"
               target="_blank"
               title="contribute"
-              >issue board</a
-            >.
+            >issue board</a>.
           </p>
           <p>
             Thank you for developing with Vuetify and I look forward to bringing
@@ -42,15 +54,23 @@
             </em>
           </div>
           <hr class="my-3" />
-          <a href="https://nuxtjs.org/" target="_blank">Nuxt Documentation</a>
+          <a
+            href="https://nuxtjs.org/"
+            target="_blank"
+          >Nuxt Documentation</a>
           <br />
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank"
-            >Nuxt GitHub</a
-          >
+          <a
+            href="https://github.com/nuxt/nuxt.js"
+            target="_blank"
+          >Nuxt GitHub</a>
         </v-card-text>
         <v-card-actions>
           <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire">Continue</v-btn>
+          <v-btn
+            color="primary"
+            nuxt
+            to="/inspire"
+          >Continue</v-btn>
         </v-card-actions>
       </v-card>
     </v-flex>
